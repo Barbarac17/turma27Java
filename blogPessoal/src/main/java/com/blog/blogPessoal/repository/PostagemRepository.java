a92cb78d3ae76cdfe330ead50e3a32c13b48c085
Barbarac17 <barbara.cirilo17@gmail.com>
2021-07-28 19:56:34 -0300

    blog

--- blogPessoal/src/main/java/com/blog/blogPessoal/repository/PostagemRepository.java
+++ blogPessoal/src/main/java/com/blog/blogPessoal/repository/PostagemRepository.java
@@ -1,5 +1,15 @@
 package com.blog.blogPessoal.repository;
 
-public interface PostagemRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.blog.blogPessoal.model.Postagem;
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo); 
+
+	
 }
