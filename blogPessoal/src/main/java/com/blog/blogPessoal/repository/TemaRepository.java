ca07b15a7a9425aa03f4ffb7ae08d0640deb7b83
Barbarac17 <barbara.cirilo17@gmail.com>
2021-07-29 18:53:46 -0300

    blog p fim

---
+++ blogPessoal/src/main/java/com/blog/blogPessoal/repository/TemaRepository.java
@@ -0,0 +1,14 @@
+package com.blog.blogPessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.blog.blogPessoal.model.Tema;
+
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+	
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+	
+
+}
